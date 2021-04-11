fb047332aa4653bdc76f635ec4dfae398dca0268
Jacob Barrett <jsbarrett88@gmail.com>
2021-04-10 23:05:42 -0500

    Setup sidenav

--- index.js
+++ index.js
@@ -41,10 +41,41 @@ Vue.component('app-root', {
   template: `
     <div class="flex flex-col xl:flex-row">
       <!-- SIDE NAVIGATION -->
-      <nav class="h-20 xl:h-auto xl:min-h-screen xl:w-24 bg-gray-300"></nav>
+      <nav class="h-20 w-full xl:h-auto xl:min-h-screen xl:w-24 bg-gray-300 flex xl:flex-col items-center shadow-lg fixed top-0 left-0">
+        <div
+          class="ml-4 xl:ml-0 xl:mt-4 w-16 h-16 text-5xl border-8 flex items-center justify-center pt-1 leading-1 font-bold rounded-full border-solid border-black">
+          P
+        </div>
+
+        <div class="xl:mt-8 flex flex-grow xl:flex-col xl:max-h-screen justify-around xl:justify-start mx-auto">
+          <div class="py-8">
+            <svg xmlns="http://www.w3.org/2000/svg" class="ml-4 xl:ml-0 h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+            </svg>
+          </div>
+
+          <div class="py-8">
+            <svg xmlns="http://www.w3.org/2000/svg" class="ml-4 xl:ml-0 h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
+            </svg>
+          </div>
+
+          <div class="py-8">
+            <svg xmlns="http://www.w3.org/2000/svg" class="ml-4 xl:ml-0 h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+            </svg>
+          </div>
+
+          <div class="py-8">
+            <svg xmlns="http://www.w3.org/2000/svg" class="ml-4 xl:ml-0 h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+            </svg>
+          </div>
+        </div>
+      </nav>
 
       <!-- MAIN -->
-      <main class="flex-grow">
+      <main class="flex-grow mt-20 xl:mt-0 xl:ml-24">
         <!-- HEADER -->
         <header class="mt-32 text-center flex flex-col items-center">
           <h1 style="font-size: 12rem; line-height: 1;" class="border-8 border-solid border-black p-4">
