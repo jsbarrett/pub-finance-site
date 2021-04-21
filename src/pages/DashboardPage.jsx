6a140d17467ef2d9fce121cc068ee39a44e74754
Jacob Barrett <jsbarrett88@gmail.com>
2021-04-21 01:28:25 -0500

    Add background swirl

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -261,10 +261,16 @@ export const DashboardPage = () => {
 
   return (
     <div style={{ backgroundColor: 'rgb(11, 19, 43)' }} className='text-white'>
-      <header className='relative pt-48 pb-32 text-center flex flex-col items-center'>
+      <header className='relative pt-48 pb-72 text-center flex flex-col items-center'>
         <div
           className='w-full h-full absolute opacity-50 top-0'
           style={{ background: 'linear-gradient(0deg, rgba(11,19,43,1) 0%, rgba(68,255,1,1) 100%)' }}>
+        </div>
+        <div className='bottom-0 absolute w-full'>
+          <svg viewBox="0 0 900 200" xmlns="http://www.w3.org/2000/svg" version="1.1">
+            <rect x="0" y="0" width="900" height="200" fill="none"></rect>
+            <path d="M0 160L16.7 139.8C33.3 119.7 66.7 79.3 100 75.2C133.3 71 166.7 103 200 111.7C233.3 120.3 266.7 105.7 300 104.8C333.3 104 366.7 117 400 111.3C433.3 105.7 466.7 81.3 500 67.2C533.3 53 566.7 49 600 65C633.3 81 666.7 117 700 121.7C733.3 126.3 766.7 99.7 800 88.7C833.3 77.7 866.7 82.3 883.3 84.7L900 87L900 201L883.3 201C866.7 201 833.3 201 800 201C766.7 201 733.3 201 700 201C666.7 201 633.3 201 600 201C566.7 201 533.3 201 500 201C466.7 201 433.3 201 400 201C366.7 201 333.3 201 300 201C266.7 201 233.3 201 200 201C166.7 201 133.3 201 100 201C66.7 201 33.3 201 16.7 201L0 201Z" fill="rgb(11, 19, 43)" strokeLinecap="round" strokeLinejoin="miter"></path>
+          </svg>
         </div>
         <svg className='relative z-10' width='403' height='195' viewBox='0 0 403 195' fill='none' xmlns='http://www.w3.org/2000/svg'>
           <path d='M121.197 164.557C118.402 161.532 117.358 157.505 118.125 152.912L118.694 149.728C119.17 147.126 119.615 144.666 119.984 142.199V142H30V167H124C122.994 166.278 122.055 165.46 121.197 164.557Z' fill='white'/>
@@ -277,7 +283,7 @@ export const DashboardPage = () => {
         </svg>
       </header>
 
-      <section className='flex flex-wrap justify-between mb-32 w-9/12 mx-auto'>
+      <section className='relative flex flex-wrap justify-between -mt-36 mb-32 w-9/12 mx-auto'>
         <div
           className='my-8 w-full lg:w-5/12 rounded-3xl bg-gray-300 shadow-xl'
           style={{ background: 'rgb(12,12,97)' }}>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,10 +10,16 @@ import { ACCENT_GREEN } from '../styles'
 export const HomePage = () => {
   return (
     <div style={{ backgroundColor: 'rgb(11, 19, 43)' }} className='text-white'>
-      <header className='relative pt-32 text-center flex flex-col items-center'>
+      <header className='relative pt-32 text-center flex flex-col items-center pb-96'>
         <div
           className='w-full h-full absolute opacity-50 top-0'
           style={{ background: 'linear-gradient(0deg, rgba(11,19,43,1) 0%, rgba(68,255,1,1) 100%)' }}>
+        </div>
+        <div className='bottom-0 absolute w-full'>
+          <svg viewBox="0 0 900 200" xmlns="http://www.w3.org/2000/svg" version="1.1">
+            <rect x="0" y="0" width="900" height="200" fill="none"></rect>
+            <path d="M0 160L16.7 139.8C33.3 119.7 66.7 79.3 100 75.2C133.3 71 166.7 103 200 111.7C233.3 120.3 266.7 105.7 300 104.8C333.3 104 366.7 117 400 111.3C433.3 105.7 466.7 81.3 500 67.2C533.3 53 566.7 49 600 65C633.3 81 666.7 117 700 121.7C733.3 126.3 766.7 99.7 800 88.7C833.3 77.7 866.7 82.3 883.3 84.7L900 87L900 201L883.3 201C866.7 201 833.3 201 800 201C766.7 201 733.3 201 700 201C666.7 201 633.3 201 600 201C566.7 201 533.3 201 500 201C466.7 201 433.3 201 400 201C366.7 201 333.3 201 300 201C266.7 201 233.3 201 200 201C166.7 201 133.3 201 100 201C66.7 201 33.3 201 16.7 201L0 201Z" fill="rgb(11, 19, 43)" strokeLinecap="round" strokeLinejoin="miter"></path>
+          </svg>
         </div>
         <svg className='relative z-10' width='403' height='195' viewBox='0 0 403 195' fill='none' xmlns='http://www.w3.org/2000/svg'>
           <path d='M121.197 164.557C118.402 161.532 117.358 157.505 118.125 152.912L118.694 149.728C119.17 147.126 119.615 144.666 119.984 142.199V142H30V167H124C122.994 166.278 122.055 165.46 121.197 164.557Z' fill='white'/>
@@ -29,7 +35,7 @@ export const HomePage = () => {
         </p>
       </header>
 
-      <section className='mt-36 space-y-16 xl:space-y-0 xl:space-x-16 px-12 flex flex-col xl:flex-row justify-between max-w-screen-2xl mx-auto'>
+      <section className='-mt-60 relative space-y-16 xl:space-y-0 xl:space-x-16 px-12 flex flex-col xl:flex-row justify-between max-w-screen-2xl mx-auto'>
         <div
           className='px-8 py-6 rounded-3xl shadow-xl w-full flex flex-col justify-between'
           style={{ background: 'linear-gradient(180deg, #0C0C61 0%, #05052D 200%)' }}>
