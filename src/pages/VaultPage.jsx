0952efbf2dce3c05aa9d36220e1c527b262fd1a5
Jacob Barrett <jsbarrett88@gmail.com>
2021-04-27 17:24:06 -0500

    Remove import of useState so it'll compile

--- src/pages/VaultPage.jsx
+++ src/pages/VaultPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { PageFooter } from '../components/PageFooter'
 import { SectionPadding } from '../components/SectionPadding'
 import { HeaderBackground } from '../components/HeaderBackground'
