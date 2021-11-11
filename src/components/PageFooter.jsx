e6067cec3adaf6267bd140a8610197dcf399474e
Jacob Barrett <jsbarrett88@gmail.com>
2021-11-11 12:42:27 -0600

    Add discord link to footer

--- src/components/PageFooter.jsx
+++ src/components/PageFooter.jsx
@@ -5,23 +5,23 @@ export const PageFooter = () => {
   return (
     <footer className='relative'>
       <div
-        className='w-full h-full absolute opacity-50 top-0'
+        className='absolute top-0 w-full h-full opacity-50'
         style={{ background: 'linear-gradient(0deg, rgba(68,255,1,1) 0%, rgba(11,19,43,1) 100%)' }}>
       </div>
 
-      <svg className='xl:-mt-20 absolute z-10 transform rotate-180 w-full h-full' preserveAspectRatio="none" viewBox="0 0 900 200" xmlns="http://www.w3.org/2000/svg" version="1.1">
+      <svg className='absolute z-10 w-full h-full xl:-mt-20 transform rotate-180' preserveAspectRatio="none" viewBox="0 0 900 200" xmlns="http://www.w3.org/2000/svg" version="1.1">
         <rect x="0" y="0" width="900" height="200" fill="none"></rect>
         <path d="M0 160L16.7 139.8C33.3 119.7 66.7 79.3 100 75.2C133.3 71 166.7 103 200 111.7C233.3 120.3 266.7 105.7 300 104.8C333.3 104 366.7 117 400 111.3C433.3 105.7 466.7 81.3 500 67.2C533.3 53 566.7 49 600 65C633.3 81 666.7 117 700 121.7C733.3 126.3 766.7 99.7 800 88.7C833.3 77.7 866.7 82.3 883.3 84.7L900 87L900 201L883.3 201C866.7 201 833.3 201 800 201C766.7 201 733.3 201 700 201C666.7 201 633.3 201 600 201C566.7 201 533.3 201 500 201C466.7 201 433.3 201 400 201C366.7 201 333.3 201 300 201C266.7 201 233.3 201 200 201C166.7 201 133.3 201 100 201C66.7 201 33.3 201 16.7 201L0 201Z" fill="rgb(11, 19, 43)" strokeLinecap="round" strokeLinejoin="miter"></path>
       </svg>
 
-      <div className='space-y-4 text-center text-xl h-96 flex flex-col items-center justify-center '>
-        <div className='flex space-x-4 relative z-10'>
+      <div className='flex flex-col items-center justify-center text-xl text-center space-y-4 h-96 '>
+        <div className='relative z-10 flex space-x-4'>
           <a
             href='https://etherscan.io/address/0x492213928481333179bE2Fe6D7fa8e0AA4c7737D#code'
             target='_blank'
             rel='noreferrer'
             style={{ borderWidth: '2px' }}
-            className='border-solid border-white p-2 rounded-full flex items-center justify-center'>
+            className='flex items-center justify-center p-2 border-white border-solid rounded-full'>
             <EtherscanLogoSvg />
           </a>
           <a
@@ -29,8 +29,8 @@ export const PageFooter = () => {
             target='_blank'
             rel='noreferrer'
             style={{ borderWidth: '2px' }}
-            className='border-solid border-white p-2 rounded-full flex items-center justify-center'>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" viewBox="0 0 128 128">
+            className='flex items-center justify-center p-2 border-white border-solid rounded-full'>
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8" viewBox="0 0 128 128">
               <g fill="none" fillRule="evenodd">
                 <circle cx="64" cy="64" r="64" fill="none" fillRule="nonzero"/>
                 <path fill='currentColor' fillRule="nonzero" d="M28.9700376,63.3244248 C47.6273373,55.1957357 60.0684594,49.8368063 66.2934036,47.2476366 C84.0668845,39.855031 87.7600616,38.5708563 90.1672227,38.528 C90.6966555,38.5191258 91.8804274,38.6503351 92.6472251,39.2725385 C93.294694,39.7979149 93.4728387,40.5076237 93.5580865,41.0057381 C93.6433345,41.5038525 93.7494885,42.63857 93.6651041,43.5252052 C92.7019529,53.6451182 88.5344133,78.2034783 86.4142057,89.5379542 C85.5170662,94.3339958 83.750571,95.9420841 82.0403991,96.0994568 C78.3237996,96.4414641 75.5015827,93.6432685 71.9018743,91.2836143 C66.2690414,87.5912212 63.0868492,85.2926952 57.6192095,81.6896017 C51.3004058,77.5256038 55.3966232,75.2369981 58.9976911,71.4967761 C59.9401076,70.5179421 76.3155302,55.6232293 76.6324771,54.2720454 C76.6721165,54.1030573 76.7089039,53.4731496 76.3346867,53.1405352 C75.9604695,52.8079208 75.4081573,52.921662 75.0095933,53.0121213 C74.444641,53.1403447 65.4461175,59.0880351 48.0140228,70.8551922 C45.4598218,72.6091037 43.1463059,73.4636682 41.0734751,73.4188859 C38.7883453,73.3695169 34.3926725,72.1268388 31.1249416,71.0646282 C27.1169366,69.7617838 23.931454,69.0729605 24.208838,66.8603276 C24.3533167,65.7078514 25.9403832,64.5292172 28.9700376,63.3244248 Z"/>
@@ -42,16 +42,33 @@ export const PageFooter = () => {
             target='_blank'
             rel='noreferrer'
             style={{ borderWidth: '2px' }}
-            className='border-solid border-white p-2 rounded-full flex items-center justify-center'>
-            <svg viewBox="0 0 36 36" className="h-8 w-8" fill="currentColor" stroke='currentColor' style={{ transform: 'translate(15%, 20%)' }}>
+            className='flex items-center justify-center p-2 border-white border-solid rounded-full'>
+            <svg viewBox="0 0 36 36" className="w-8 h-8" fill="currentColor" stroke='currentColor' style={{ transform: 'translate(15%, 20%)' }}>
               <g>
                 <path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path>
               </g>
             </svg>
           </a>
+          <a
+            href='https://discord.gg/ZSmdZvAKda'
+            target='_blank'
+            rel='noreferrer'
+            style={{ borderWidth: '2px' }}
+            className='flex items-center justify-center p-2 border-white border-solid rounded-full'>
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" viewBox="0 0 71 55" fill="none">
+              <g clip-path="url(#clip0)">
+              <path d="M60.1045 4.8978C55.5792 2.8214 50.7265 1.2916 45.6527 0.41542C45.5603 0.39851 45.468 0.440769 45.4204 0.525289C44.7963 1.6353 44.105 3.0834 43.6209 4.2216C38.1637 3.4046 32.7345 3.4046 27.3892 4.2216C26.905 3.0581 26.1886 1.6353 25.5617 0.525289C25.5141 0.443589 25.4218 0.40133 25.3294 0.41542C20.2584 1.2888 15.4057 2.8186 10.8776 4.8978C10.8384 4.9147 10.8048 4.9429 10.7825 4.9795C1.57795 18.7309 -0.943561 32.1443 0.293408 45.3914C0.299005 45.4562 0.335386 45.5182 0.385761 45.5576C6.45866 50.0174 12.3413 52.7249 18.1147 54.5195C18.2071 54.5477 18.305 54.5139 18.3638 54.4378C19.7295 52.5728 20.9469 50.6063 21.9907 48.5383C22.0523 48.4172 21.9935 48.2735 21.8676 48.2256C19.9366 47.4931 18.0979 46.6 16.3292 45.5858C16.1893 45.5041 16.1781 45.304 16.3068 45.2082C16.679 44.9293 17.0513 44.6391 17.4067 44.3461C17.471 44.2926 17.5606 44.2813 17.6362 44.3151C29.2558 49.6202 41.8354 49.6202 53.3179 44.3151C53.3935 44.2785 53.4831 44.2898 53.5502 44.3433C53.9057 44.6363 54.2779 44.9293 54.6529 45.2082C54.7816 45.304 54.7732 45.5041 54.6333 45.5858C52.8646 46.6197 51.0259 47.4931 49.0921 48.2228C48.9662 48.2707 48.9102 48.4172 48.9718 48.5383C50.038 50.6034 51.2554 52.5699 52.5959 54.435C52.6519 54.5139 52.7526 54.5477 52.845 54.5195C58.6464 52.7249 64.529 50.0174 70.6019 45.5576C70.6551 45.5182 70.6887 45.459 70.6943 45.3942C72.1747 30.0791 68.2147 16.7757 60.1968 4.9823C60.1772 4.9429 60.1437 4.9147 60.1045 4.8978ZM23.7259 37.3253C20.2276 37.3253 17.3451 34.1136 17.3451 30.1693C17.3451 26.225 20.1717 23.0133 23.7259 23.0133C27.308 23.0133 30.1626 26.2532 30.1066 30.1693C30.1066 34.1136 27.28 37.3253 23.7259 37.3253ZM47.3178 37.3253C43.8196 37.3253 40.9371 34.1136 40.9371 30.1693C40.9371 26.225 43.7636 23.0133 47.3178 23.0133C50.9 23.0133 53.7545 26.2532 53.6986 30.1693C53.6986 34.1136 50.9 37.3253 47.3178 37.3253Z" fill="#ffffff"/>
+              </g>
+              <defs>
+              <clipPath id="clip0">
+              <rect width="71" height="55" fill="white"/>
+              </clipPath>
+              </defs>
+            </svg>
+          </a>
         </div>
 
-        <div className='text-sm xl:text-normal relative z-10'>
+        <div className='relative z-10 text-sm xl:text-normal'>
           &#169; 2021 Pub Finance. All rights reserved.
         </div>
       </div>
