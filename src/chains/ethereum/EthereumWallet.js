7f45275aa12e1258525492f095223e28d1c9ec90
Jacob Barrett <jsbarrett88@gmail.com>
2022-03-29 14:10:51 -0500

    Lock down rest of dependencies for now

--- src/chains/ethereum/EthereumWallet.js
+++ src/chains/ethereum/EthereumWallet.js
@@ -39,7 +39,7 @@ const sendTokens = async ({ address, amount, w3 }) => {
 
   // approve
   await PubContract.methods
-    .approve(ethereumPubAddress, new BigNumber(amount))
+    .approve(ethereumPubAddress, amount)
     .send({ from: address })
 
   const payload = ethers.utils.defaultAbiCoder
@@ -63,7 +63,7 @@ const sendTokens = async ({ address, amount, w3 }) => {
 
   // sendTokens
   await PubContract.methods
-    .sendTokens(avaxChainId, avaxPubAddress, new BigNumber(amount))
+    .sendTokens(avaxChainId, avaxPubAddress, amount)
     .send({ from: address, value: fee })
 }
 
