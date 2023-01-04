178b1c27601cdb5f93cb1fb28caa9639737a4668
Jacob Barrett <jsbarrett88@gmail.com>
2023-01-04 15:03:07 -0600

    Update Goerli Vault

---
+++ src/pages/VaultPage/GoerliVault/GoerliVaultFactory.jsx
@@ -0,0 +1,138 @@
+import { useState, useEffect } from 'react'
+import { HeaderBackground } from '../../../components/HeaderBackground'
+import { PageFooter } from '../../../components/PageFooter'
+import { SectionPadding } from '../../../components/SectionPadding'
+
+export const GoerliVaultFactory = ({ backend }) => {
+  const [amountStaked, setAmountStaked] = useState('0')
+  const [unclaimedRewards, setUnclaimedRewards] = useState('0')
+  const [addStakeAmount, setAddStakeAmount] = useState('0')
+  const [pintBalance, setPintBalance] = useState('0')
+
+  // TODO: error handling
+  useEffect(() => {
+    backend.getAmountStaked().then(setAmountStaked)
+    backend.getUnclaimedRewards().then(setUnclaimedRewards)
+    backend.getPINTBalance().then(setPintBalance)
+  }, [])
+
+  // TODO: error handling
+  const stakePint = async () => {
+    try {
+      await backend.stakePINT(addStakeAmount) 
+      await backend.getAmountStaked().then(setAmountStaked)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  return (
+    <GoerliVaultView
+      amountStaked={amountStaked}
+      unclaimedRewards={unclaimedRewards}
+      stakePint={stakePint}
+      unstakePint={backend.unstakePINT}
+      claimRewards={backend.claimRewards}
+      addStakeAmount={addStakeAmount}
+      setAddStakeAmount={setAddStakeAmount}
+      mintPINT={backend.mintPINT}
+      pintBalance={pintBalance}
+    />
+  )
+}
+
+export const GoerliVaultView = ({
+  stakePint,
+  unstakePint,
+  amountStaked,
+  unclaimedRewards,
+  claimRewards,
+  addStakeAmount,
+  setAddStakeAmount,
+  mintPINT,
+  pintBalance,
+}) => {
+  return (
+    <div style={{ backgroundColor: 'rgb(11, 19, 43)' }} className='relative text-white'>
+      <header className='relative flex flex-col items-center pb-48 text-center pt-36 xl:pt-48 xl:pb-60'>
+        <HeaderBackground />
+        <h1
+          className='relative z-10 text-6xl font-bold leading-none xl:text-8xl'>
+          Vaults (Goerli)
+        </h1>
+        <p className='relative z-10 mt-10 text-2xl xl:text-4xl xl:mt-16'>
+          Flexible Deposits, Higher Profits.
+        </p>
+      </header>
+
+      <SectionPadding>
+        <section className='mx-auto mb-24 max-w-screen-2xl xl:mb-48'>
+          <div className='flex justify-center'>
+            <div
+              className='px-16 py-16 rounded-lg'
+              style={{ background: 'linear-gradient(180deg, #0C0C61 0%, #05052D 200%)' }}>
+
+              <div className='text-3xl font-bold text-center'>Stake PINT</div>
+
+              <div className='mt-16'>
+                <div className='opacity-80'>balance:</div>
+                <div className='flex justify-between'>
+                  <div className='text-4xl'>{pintBalance} PINT</div>
+                </div>
+              </div>
+
+              <div className='mt-4'>
+                <div className='opacity-80'>amount staked:</div>
+                <div className='flex justify-between'>
+                  <div className='text-4xl'>{amountStaked} PINT</div>
+                  <button
+                    onClick={unstakePint}
+                    className='w-24 px-2 ml-4 text-gray-300 border border-gray-300 rounded'>
+                    unstake
+                  </button>
+                </div>
+              </div>
+
+              <div className='flex justify-between mt-8'>
+                <input
+                  placeholder={'Add Stake'}
+                  className='px-2 py-2 text-xl bg-transparent border border-gray-300 border-solid rounded'
+                  value={addStakeAmount}
+                  onChange={evt => setAddStakeAmount(evt.target.value) }
+                />
+                <button
+                  onClick={stakePint}
+                  className='w-24 px-2 ml-4 font-bold border rounded text-accent-green border-accent-green'>
+                  stake
+                </button>
+              </div>
+
+              <button
+                onClick={mintPINT}
+                className='mt-2 text-gray-400'>
+                (click here if you need to mint some goerli PINT)
+              </button>
+
+              <hr className='my-16 border' />
+
+              <div className='opacity-80'>unclaimed rewards:</div>
+              <div className='flex justify-between'>
+                (not available to show from this page yet ... coming soon)
+                {/* <div className='text-4xl text-accent-green'>{unclaimedRewards} PINT</div> */}
+                {/* <button */}
+                {/*   onClick={claimRewards} */}
+                {/*   className='w-24 px-2 ml-4 font-bold border rounded text-accent-green border-accent-green'> */}
+                {/*   claim */}
+                {/* </button> */}
+              </div>
+
+            </div>
+          </div>
+        </section>
+      </SectionPadding>
+
+      <PageFooter />
+    </div>
+  )
+}
+
---
+++ src/pages/VaultPage/GoerliVault/index.jsx
@@ -0,0 +1,137 @@
+import { GoerliVaultFactory } from './GoerliVaultFactory'
+import { ethers } from 'ethers'
+import stakePINTArtifacts from './StakePINT.json'
+import fakePINTArtifacts from './FakePINT.json'
+
+// -----------------------------------------------------------------------------
+// CONSTANTS
+// -----------------------------------------------------------------------------
+
+const STAKEPINT_CONTRACT_ADDRESS = '0x9069E0447770E8F18cc97636269e8494CF108d19'
+const STAKEPINT_ABI = stakePINTArtifacts.abi
+const FAKEPINT_ADDRESS = '0x097E9EeFc58c9648bAc09050b8E12c7F1F522879'
+const FAKEPINT_ABI = fakePINTArtifacts.abi
+
+// -----------------------------------------------------------------------------
+// UTILS
+// -----------------------------------------------------------------------------
+
+const convertPINTForDisplay = x => (Number(x / (10n ** 16n)) / 100).toLocaleString()
+const convertDisplayValueToPINTForContract = x => BigInt(x) * 10n ** 18n
+
+// -----------------------------------------------------------------------------
+// DATA BACKEND
+// -----------------------------------------------------------------------------
+
+const getAmountStaked = async () => {
+  try {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const stakePINTContract = new ethers.Contract(STAKEPINT_CONTRACT_ADDRESS, STAKEPINT_ABI, provider)
+
+    const stakedPINT = await stakePINTContract.getAmountStaked(window.ethereum.selectedAddress)
+
+    return convertPINTForDisplay(stakedPINT.toBigInt())
+  } catch (err) {
+    console.error(err)
+    alert('Sorry there was a problem retrieving the amount of your staked PINT')
+
+    return '0'
+  }
+}
+
+const getUnclaimedRewards = async () => {
+  try {
+    return '1'
+  } catch (err) {
+    console.error(err)
+    alert('Sorry there was a problem retrieving the amount of your rewards')
+
+    return '0'
+  }
+}
+
+const stakePINT = async (amount) => {
+  try {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    const stakePINTContract = new ethers.Contract(STAKEPINT_CONTRACT_ADDRESS, STAKEPINT_ABI, signer)
+    const fakePINTContract = new ethers.Contract(FAKEPINT_ADDRESS, FAKEPINT_ABI, signer)
+
+    alert('first we must approve the transfer')
+    await (await fakePINTContract.approve(STAKEPINT_CONTRACT_ADDRESS, convertDisplayValueToPINTForContract(amount))).wait()
+
+    alert('now we can have the staking contract transfer the funds to be staked')
+    await stakePINTContract.stake(convertDisplayValueToPINTForContract(amount))
+  } catch (err) {
+    console.error(err)
+    alert('Sorry there was a problem with staking your PINT, please reach out to support to help address your issue.')
+  }
+}
+
+const unstakePINT = async () => {
+  try {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    const stakePINTContract = new ethers.Contract(STAKEPINT_CONTRACT_ADDRESS, STAKEPINT_ABI, signer)
+
+    const stakedPINT = await stakePINTContract.getAmountStaked(window.ethereum.selectedAddress)
+    await stakePINTContract.unstake(stakedPINT)
+  } catch (err) {
+    console.error(err)
+    alert('Sorry there was a problem with unstaking your PINT, please reach out to support to help address your issue.')
+  }
+}
+
+const claimRewards = async () => {
+  alert('At this time, please reach out to a PINT admin to claim your rewards via Telegram/Discord... the development team is working on automating this process, but it requires work and an audit by a reputable security firm.')
+}
+
+const mintPINT = async () => {
+  try {
+    const ONE_PINT = 1000000000000000000n
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    const fakePINTContract = new ethers.Contract(FAKEPINT_ADDRESS, FAKEPINT_ABI, signer)
+
+    await fakePINTContract.mint(1000n * ONE_PINT)
+  } catch (err) {
+    console.error(err)
+    alert('Sorry there was a problem with minting some PINT for you')
+  }
+}
+
+const getPINTBalance = async () => {
+  try {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const fakePINTContract = new ethers.Contract(FAKEPINT_ADDRESS, FAKEPINT_ABI, provider)
+    const balance = await fakePINTContract.balanceOf(window.ethereum.selectedAddress)
+    console.log({ balance: balance.toString() })
+    return convertPINTForDisplay(balance.toBigInt())
+  } catch (err) {
+    console.error(err)
+    alert('Sorry there was a problem getting your PINT balance')
+    return '0'
+  }
+}
+
+const backend = {
+  getAmountStaked,
+  getUnclaimedRewards,
+  stakePINT,
+  unstakePINT,
+  claimRewards,
+  mintPINT,
+  getPINTBalance,
+}
+
+// -----------------------------------------------------------------------------
+// EXPORT
+// -----------------------------------------------------------------------------
+
+export const GoerliVault = () => {
+  return (
+    <GoerliVaultFactory backend={backend} />
+  )
+}
+
